247a13ba3e49b76ce92888bc2157b5d958086f3a
gustavgans <software.arnoldfranke@gmail.com>
2013-01-10 17:13:58 +0100

    Erster Listview und gitignore

--- teacherbase/src/de/franke/teacherbase/MainActivity.java
+++ teacherbase/src/de/franke/teacherbase/MainActivity.java
@@ -3,13 +3,22 @@ package de.franke.teacherbase;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
+	private String[] entries = {"Klassen", "Schüler", "Tests"};
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+				
+		ListView listview = (ListView) this.findViewById(R.id.listView1);
+		listview.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, entries));
+		
+		
 	}
 
 	@Override
